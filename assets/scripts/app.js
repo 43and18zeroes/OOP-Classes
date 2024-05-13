31288b00147593f8e9cc451e675ffd07dda86d05
Christoph <c.wagner.germany@gmail.com>
2024-05-13 21:32:05 +0200

    13May13

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,7 +9,7 @@ class DOMHelper {
     const element = document.getElementById(elementId);
     const destinationElement = document.querySelector(newDestinationSelector);
     destinationElement.append(element);
-    element.scrollIntoView(); // Scroll
+    element.scrollIntoView({behavior: 'smooth'}); // Scroll
   }
 }
 
