c0f34d4e2dc7416fe423769aef94668d7d84f75b
Christoph <c.wagner.germany@gmail.com>
2024-05-13 23:01:36 +0200

    13May21

--- assets/scripts/analytics.js
+++ assets/scripts/analytics.js
@@ -1,3 +1,9 @@
-setInterval(() => {
-    console.log('Sending analytics...')
-}, 2000);
+// setInterval(() => {
+//   console.log('Sending analytics...');
+// }, 2000);
+
+setInterval(printAnalytics, 2000);
+
+function printAnalytics() {
+    console.log('Sending analytics...');
+}
