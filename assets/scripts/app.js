2d816e24249767a06407b76bfec63d0b4ae70169
Christoph <c.wagner.germany@gmail.com>
2024-05-17 18:27:48 +0200

    17May2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -175,7 +175,16 @@ class ProjectList {
 
     list.addEventListener('drop', event => {
       const prjId = event.dataTransfer.getData('text/plain');
-    })
+      if (this.projects.find(p => p.id === prjId)) {
+        return;
+      }
+      document
+        .getElementById(prjId)
+        .querySelector('button:last-of-type')
+        .click();
+      list.parentElement.classList.remove('droppable');
+      event.preventDefault();
+    });
   }
 
   setSwitchHandlerFunction(switchHandlerFunction) {
